1bd9e643a7cd7e711a42e833198d0d563e232664
Mohamed Mansour <hello@mohamedmansour.com>
2011-04-30 20:33:05 -0400

    Refactor JS into its own Controller and reuse some code.

---
+++ js/preview_controller.js
@@ -0,0 +1,130 @@
+// Copyright 2011 Mohamed Mansour. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
+
+/**
+ * Controlls the Preview Page.
+ * @constructor
+ */
+PreviewController = function()
+{
+  this.preview = null;
+};
+
+/**
+ * Initializes listeners for the buttons in the previewer.
+ */
+PreviewController.prototype.init = function()
+{
+  var self = this;
+  // Listen for extension requests.
+  chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
+    if (req.method == 'SetImage') {
+      // Startup the previewer to do its business!
+      self.preview = new WallpaperPreview('crx_wlp_canvas', req.data.image,
+                                           PositionEnum.valueOf(req.data.position));
+      // Send a call natively to inform the operating system about its color.
+      chrome.extension.sendRequest({method: 'GetSystemColor'}, function(res) {
+        self.preview.setCanvasBackground(res.color);
+      });
+      // Set the appropriate item selected.
+      var i = $('crx_wlp_' + req.data.position + 'Button');
+      i.className = 'selected';
+    }
+    else if (req.method = 'IsWindows7') {
+      if (req.data) {
+        self.setupWindows7Components();
+      }
+    }
+    sendResponse({});
+  });
+
+  // Add button click event listeners.
+  $('crx_wlp_stretchButton').addEventListener('click', function() {
+    self.changeSelection(this, PositionEnum.STRETCH);
+  }, false);
+  $('crx_wlp_tileButton').addEventListener('click', function() {
+    self.changeSelection(this, PositionEnum.TILE);
+  }, false);
+  $('crx_wlp_centerButton').addEventListener('click', function() {
+    self.changeSelection(this, PositionEnum.CENTER);
+  }, false);
+  $('crx_wlp_fillButton').addEventListener('click', function() {
+    self.changeSelection(this, PositionEnum.FILL);
+  }, false);
+  $('crx_wlp_fitButton').addEventListener('click', function() {
+    self.changeSelection(this, PositionEnum.FIT);
+  }, false);
+  $('wallpaper_submit').addEventListener('click', function() {
+    self.doSubmit();
+  }, false);
+  $('wallpaper_options').addEventListener('click', function() {
+    self.doOptions();
+  }, false);
+  $('wallpaper_close').addEventListener('click', function() {
+    self.doClose();
+  }, false);
+};
+
+/**
+ * Change the selection of the wallpaper style.
+ *
+ * @param {HTMLElement} dom The current list item.
+ * @param {Object<PositionEnum>} position The wallpaper position to set.
+ */
+PreviewController.prototype.changeSelection = function(dom, position)
+{
+  var lists = document.getElementsByTagName('li');
+  for (var i in lists) {
+    var list = lists[i];
+    list.className = '';
+  }
+  dom.className = 'selected';
+  this.preview.render(position);
+};
+  
+/**
+ * Submit a request to change the wallpaper.
+ */
+PreviewController.prototype.doSubmit = function()
+{
+  chrome.extension.sendRequest({
+      method: 'SetWallpaper', 
+      data: {
+        image: this.preview.getImageURL(),
+        position: this.preview.getPosition()
+      }
+  });
+};
+
+/**
+ * View wallpaper extension options.
+ */
+PreviewController.prototype.doOptions = function()
+{
+   chrome.extension.sendRequest({method: 'OpenOptions'});
+};
+
+/**
+ * Close IFrame Preview by sending a request back to the main extension.
+ */
+PreviewController.prototype.doClose = function()
+{
+  if (parent) {
+    chrome.extension.sendRequest({method: 'RemoveOverlay'});
+  }
+  else {
+    window.close();
+  }
+};
+
+/**
+ * Lets add Win7 specific positions and icons.
+ */
+PreviewController.prototype.setupWindows7Components = function()
+{
+  var fillButton = $('crx_wlp_fillButton');
+  var fitButton = $('crx_wlp_fitButton');
+  fillButton.style.display = 'inline-block';
+  fitButton.style.display = 'inline-block';
+};
