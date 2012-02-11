4b0104decc5e39027441133f307fadf70acdeafe
Mohamed Mansour <hello@mohamedmansour.com>
2012-02-11 02:27:00 -0500

    Implement the Extension Verification based on white/black list.

--- js/external_service.js
+++ js/external_service.js
@@ -13,11 +13,79 @@ ExternalService.prototype.init = function() {
 };
 
 /**
- * Fired when an external request came in.
+ * Fired when an external request came in verify access if it belongs in the whitelist.
+ *
+ * @param {object} request The payload that contains the request obj.
+ * @param {object} sender The initiator who sent this request.
+ * @param {Function<object>} sendResponse The callback to send back.
  */
 ExternalService.prototype.onRequestExternalListener = function(request, sender, sendResponse) {
   var extensionID = sender.id;
+  if (!this.contains(settings.whitelisted, extensionID)) {
+    if (this.contains(settings.blacklisted, extensionID)) {
+      this.denied(extensionID, request, sendResponse);
+    }
+    else {
+      this.verify(extensionID, function(verified) {
+        if (verified) {
+          this.success(extensionID, request, sendResponse);
+        }
+        else {
+          this.denied(extensionID, request, sendResponse);
+        }
+      }.bind(this));
+    }
+  }
+  else {
+    this.success(extensionID, request, sendResponse);
+  }
+};
+
+/**
+ * Success access callback.
+ *
+ * @param {string} extensionID The extension ID.
+ * @param {object} request The payload that contains the request obj.
+ * @param {Function<object>} sendResponse The callback to send back.
+ */
+ExternalService.prototype.success = function(extensionID, request, sendResponse) {
+  sendResponse({status: true, message: 'WallpaperSet'});
+};
+
+/**
+ * Denied access callback.
+ *
+ * @param {string} extensionID The extension ID.
+ * @param {object} request The payload that contains the request obj.
+ * @param {Function<object>} sendResponse The callback to send back.
+ */
+ExternalService.prototype.denied = function(extensionID, request, sendResponse) {
+  sendResponse({status: false, message: 'AccessDenied'});
+};
+
+/**
+ * Checks if the list contains the extensionID.
+ *
+ * @param {Array<string>} list The list of extension ids.
+ * @param {string} extensionID The extension ID.
+ */
+ExternalService.prototype.contains = function(list, extensionID) {
+  var found = false;
+  list.some(function(elt, idx) {
+    found = (elt == extensionID);
+    return found;
+  });
+  return found;
+};
+
+/**
+ * Verifies whether the extension could have access.
+ *
+ * @param {string} extensionID The extension ID.
+ * @param {Function<object>} callback The callback to send back when verified.
+ */
+ExternalService.prototype.verify = function(extensionID, callback) {
   chrome.management.get(extensionID, function(extensionInfo) {
-    
+    callback(true);
   });
 };
